8536eefd9b3e044642212647404e0786753f5abf
kbatchelor <kbatchelor@insidesales.com>
2017-01-11 09:28:24 -0700

    vs

--- example/app.js
+++ example/app.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import ShQuill from '../bin/sh-quill'
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            reason: '<div>first</div>'
+        };
+
+
+    }
     render() {
+        setTimeout(() => {
+            this.setState({reason:'<div>loaded</div>'})
+        }, 500);
+
         return (
             <div className="example">
-                <ShQuill className="sm"/>
+                <ShQuill className="sm" value={this.state.reason}/>
             </div>
         )
     }
