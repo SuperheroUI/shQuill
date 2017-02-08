5de2ffc5490651833189f3f762dfb03f33481bba
kevinlbatchelor <kevinbatchelor@gmail.com>
2017-02-08 10:07:49 -0700

    update example

--- example/app.js
+++ example/app.js
@@ -6,18 +6,30 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            reason: '<div>first</div>'
+            reason: '<div>beginning text</div>'
         };
 
-
+        this.handleChange = this.handleChange.bind(this);
     }
+
+    handleChange() {
+        this.setState({
+            reason: `<ul>
+                <li>one</li>
+                <li>two</li>
+                <li>three</li>
+                <li>four</li>
+                        </ul>`
+        })
+    }
+
     render() {
         return (
             <div className="">
-            <div className="example">
-                <ShQuill className="sm" value={this.state.reason}/>
-            </div>
-            <button>test</button>
+                <div className="example">
+                    <ShQuill className="sm" value={this.state.reason}/>
+                </div>
+                <button onClick={this.handleChange}>test</button>
             </div>
         )
     }
