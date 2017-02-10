db600a2f1c92468aa31bf0cc5710f651674a97c7
Sam Keller <sam.keller@insidesales.com>
2017-02-10 11:38:30 -0700

    change quill to use divs instead of p's

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ module.exports.Mixin = require('./mixin');
 module.exports.Toolbar = require('./toolbar');
 let quill = require('quill');
 let Parchment = quill.import('parchment');
+let Block = quill.import('blots/block');
+Block.tagName = 'DIV';
 let FontStyle = new Parchment.Attributor.Style('size', 'font-size', {scope: Parchment.Scope.INLINE});
 let FontFamilyStyle = new Parchment.Attributor.Style('font', 'font-family', {scope: Parchment.Scope.INLINE});
 quill.register(FontStyle, true);
 quill.register(FontFamilyStyle, true);
+quill.register(Block, true);
 module.exports.Quill = quill;
 
 require('quill/dist/quill.core.css');
